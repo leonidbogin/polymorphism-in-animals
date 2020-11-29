996fc36a88bbe5f65d0ce3b78103ac7b5630d1a4
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-30 00:49:22 +0300

    Create class Dragonfly

---
+++ polymorphism-in-animals/polymorphism-in-animals/Dragonfly.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polymorphism_in_animals
+{
+    internal class Dragonfly : Animal
+    {
+        public Dragonfly(string name, int footNumber, bool hasMustache)
+            : base(name, footNumber, hasMustache)
+        {
+
+        }
+
+        public override string Voice()
+        {
+            return "бж-ж-ж";
+        }
+
+        public override string Move()
+        {
+            return "полет";
+        }
+
+        public new string IsHungry()
+        {
+            return "хочу насекомых";
+        }
+    }
+}
