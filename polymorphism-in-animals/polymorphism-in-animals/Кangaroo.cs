c0775d55dc84a3a469b081d51892ec6660a4333f
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-30 00:48:42 +0300

    Create class Кangaroo

---
+++ polymorphism-in-animals/polymorphism-in-animals/Кangaroo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polymorphism_in_animals
+{
+    internal class Кangaroo : Animal
+    {
+        public Кangaroo(string name, int footNumber, bool hasMustache)
+            : base(name, footNumber, hasMustache)
+        {
+
+        }
+
+        public override string Voice()
+        {
+            return "кхе-кхе";
+        }
+
+        public override string Move()
+        {
+            return "прыг-прыг";
+        }
+
+        public new string IsHungry()
+        {
+            return "хочу ягоды";
+        }
+    }
+}
