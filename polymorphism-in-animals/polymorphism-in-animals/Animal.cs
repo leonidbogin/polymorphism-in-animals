688dde271644e084598daef84e223bd738b029c1
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-30 00:43:11 +0300

    Create class Animal

---
+++ polymorphism-in-animals/polymorphism-in-animals/Animal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polymorphism_in_animals
+{
+    internal class Animal
+    {
+        private string Name { get; set; }
+        private int FootNumber { get; set; }
+        private bool HasMustache { get; set; }
+
+        public Animal()
+        {
+
+        }
+
+        public Animal(string name, int footNumber, bool hasMustache)
+        {
+            Name = name;
+            FootNumber = footNumber;
+            HasMustache = hasMustache;
+        }
+
+        public virtual string Voice()
+        {
+            return "голос";
+        }
+
+        public virtual string Move()
+        {
+            return "движение";
+        }
+
+        public virtual string IsHungry()
+        {
+            return "не голоден";
+        }
+    }
+}
