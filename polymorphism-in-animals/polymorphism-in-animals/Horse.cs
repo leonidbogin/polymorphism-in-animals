b065f5420a503e7a49b631daac61b8695f09c52f
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-30 00:44:40 +0300

    Create class Horse

---
+++ polymorphism-in-animals/polymorphism-in-animals/Horse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace polymorphism_in_animals
+{
+    internal class Horse : Animal
+    {
+        public Horse(string name, int footNumber, bool hasMustache)
+            : base(name, footNumber, hasMustache)
+        {
+
+        }
+
+        public override string Voice()
+        {
+            return "и-го-го";
+        }
+
+        public override string Move()
+        {
+            return "топ-топ";
+        }
+
+        public new string IsHungry()
+        {
+            return "хочу сено";
+        }
+    }
+}
