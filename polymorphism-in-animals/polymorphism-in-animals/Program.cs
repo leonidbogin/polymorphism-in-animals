85d5b52d7d256d642c8d1d4bab1b6fc959097872
leonidbogin <65702576+leonidbogin@users.noreply.github.com>
2020-11-30 00:42:56 +0300

    Init console project

---
+++ polymorphism-in-animals/polymorphism-in-animals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace polymorphism_in_animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
